1aec4255599b6c72d93d925eefeff204fff47291
Shyam prajapat <sp2643692@gmail.com>
2026-06-25 12:43:01 +0530

    1ddp question added

---
+++ DSA/DynammicProgramming/1.1DDp/q2.java
@@ -0,0 +1,16 @@
+// LeetCode 198 - House Robber
+
+class Solution {
+    public int rob(int[] nums) {
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for (int num : nums) {
+            int curr = Math.max(prev1, prev2 + num);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+}
